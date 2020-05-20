6ceaf4bae2563855893239bb3f7e11982f55818d
Alexey <Cyrus1988@ukr.net>
2020-05-20 16:47:44 +0300

    added migrations

--- config/db.php
+++ config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=sys',
+    'dsn' => 'mysql:host=localhost;dbname=task',
     'username' => 'root',
     'password' => 'admin',
     'charset' => 'utf8',
